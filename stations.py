8fcd04f991fd14db0ab7f1fed38af1aaea39c5c9
BellaKeri <isabellavssilva.ire@gmail.com>
2025-05-31 12:20:03 +0100

    First funcinal version

--- stations.py
+++ stations.py
@@ -6,22 +6,31 @@
 """Stations loader."""
 
 # import logging
-# import pdb
+import pdb
+import urllib.request
+import xml.dom.minidom
 
 __author__ = 'BellaKeri@github.com'
 __version__ = (1, 0)
 
+# The TfI URLs:
+ALL_STATIONS_URL = 'http://api.irishrail.ie/realtime/realtime.asmx/getAllStationsXML'
+
+XMLType = xml.dom.minidom.Document
+
 
 def LoadStations() -> str:
-  return '<xml>'
+  with urllib.request.urlopen(ALL_STATIONS_URL) as rail_data:
+    return rail_data.read()
 
 
-def ConvertToXML(data_xml: str) -> str:  # xml
-  return 'obj'
+def ConvertToXML(data_xml: str) -> XMLType:
+  return xml.dom.minidom.parseString(data_xml)
 
 
-def CountStations(xml: str) -> int:
-  return 10
+def CountStations(xml: XMLType) -> int:
+  all_stations = xml.getElementsByTagName('objStation')
+  return len(all_stations)
 
 
 def Main() -> None:
