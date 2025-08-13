91d313c32fb38c2c137a0914eb2b93bb979880da
Daniel Balparda <balparda@gmail.com>
2025-08-13 18:05:05 -0300

    exclude conftest.py from coverage

--- tests/run_coverage.sh
+++ tests/run_coverage.sh
@@ -6,6 +6,6 @@
 # https://coverage.readthedocs.io/
 #
 
-poetry run python3 -m coverage run --omit=__init__.py,*_test.py,*_tests.py,*/dist-packages/*,*/site-packages/*,*/tests/* -m pytest
+poetry run python3 -m coverage run --omit=__init__.py,*_test.py,conftest.py,*_tests.py,*/dist-packages/*,*/site-packages/*,*/tests/* -m pytest
 poetry run python3 -m coverage report -m
 poetry run python3 -m coverage html
